0c95421a0726dedd945f2f2212a3b29e04583ad9
andres alcocer <andresalcocer7@yahoo.com>
2021-10-12 14:47:29 -0400

    installed launch screen image for ios and andriod

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React, { useEffect } from 'react'
-
 import { StyleSheet, Text, View } from 'react-native'
 import { CLIENT_ID, CLIENT_SECRET, REDIRECT_URL } from '@env'
 import { authorize, refresh } from 'react-native-app-auth'
+import SplashScreen from 'react-native-splash-screen'
 
 const spotifyAuthConfig = {
   clientId: CLIENT_ID,
@@ -24,11 +24,13 @@ const spotifyAuthConfig = {
 }
 
 const App = () => {
+  useEffect(() => {}, [SplashScreen.hide()])
+
   useEffect(() => {
     const getAuthCode = async () => {
       try {
-        const res = await authorize(spotifyAuthConfig)
-        console.log('res', res)
+        // const res = await authorize(spotifyAuthConfig)
+        // console.log('res', res)
       } catch (error) {
         console.log('error', error)
       }
--- android/app/src/main/java/com/spotifyclone/MainActivity.java
+++ android/app/src/main/java/com/spotifyclone/MainActivity.java
@@ -1,6 +1,8 @@
 package com.spotifyclone;
+import android.os.Bundle; // here
 
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen; // here
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +13,11 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "spotifyClone";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+      SplashScreen.show(this);  // here
+      super.onCreate(savedInstanceState);
   }
 }
