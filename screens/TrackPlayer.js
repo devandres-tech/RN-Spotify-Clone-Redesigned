e9cd41ea7dfcbde6fc897d5f3cc76a39d9880532
andres alcocer <andresalcocer7@yahoo.com>
2021-11-24 13:02:06 -0500

    set TrackPlayer styles

--- screens/TrackPlayer.js
+++ screens/TrackPlayer.js
@@ -1,21 +1,27 @@
 import React from 'react'
-import {
-  View,
-  Text,
-  StatusBar,
-  Image,
-  SafeAreaView,
-  ImageBackground,
-} from 'react-native'
+import { View, Text, Image, SafeAreaView, ImageBackground } from 'react-native'
 import { useSelector, useDispatch } from 'react-redux'
 import LinearGradient from 'react-native-linear-gradient'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+import TrackPlayer from 'react-native-track-player'
 
+import * as playerActions from '../store/actions/audioPlayer'
 import { COLORS, FONTS, SIZES, icons } from '../constants'
 
-const TrackPlayer = ({ navigation }) => {
+const TrackPlayerScreen = ({ navigation }) => {
   const player = useSelector((state) => state.audioPlayer)
   const track = useSelector((state) => state.track)
   const dispatch = useDispatch()
+
+  const onPlayPauseHandler = async () => {
+    if (player.isTrackPlaying) {
+      dispatch(playerActions.pauseTrack())
+      await TrackPlayer.pause()
+    } else {
+      dispatch(playerActions.playTrack())
+      await TrackPlayer.play()
+    }
+  }
 
   return (
     <SafeAreaView style={{ backgroundColor: COLORS.black, flex: 1 }}>
@@ -38,22 +44,68 @@ const TrackPlayer = ({ navigation }) => {
             COLORS.black,
           ]}
         />
-        {/* progress bar  */}
-        <View></View>
-        {/* controls  */}
-        <View style={{ flexDirection: 'row' }}>
-          <View>
-            <Image
-              style={{ height: 35, width: 35, tintColor: COLORS.white }}
-              source={icons.prev}
-            />
-          </View>
-          <View></View>
-          <View></View>
-        </View>
       </ImageBackground>
+      {/* track info  */}
+      <View
+        style={{
+          alignItems: 'center',
+        }}
+      >
+        <Text style={{ color: COLORS.white, ...FONTS.h2 }}>
+          {player.track.title.toUpperCase()}
+        </Text>
+        <Text style={{ color: COLORS.lightGray, ...FONTS.body }}>
+          {player.track.artist.toUpperCase()}
+        </Text>
+      </View>
+      {/* progress bar  */}
+      <View></View>
+      {/* controls  */}
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'center',
+          alignItems: 'center',
+          marginTop: 40,
+        }}
+      >
+        <TouchableOpacity activeOpacity={0.7}>
+          <Image
+            style={{ height: 25, width: 25, tintColor: COLORS.white }}
+            source={icons.previous}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={onPlayPauseHandler}
+          activeOpacity={0.7}
+          style={{
+            height: 60,
+            width: 60,
+            backgroundColor: COLORS.white,
+            borderRadius: 30,
+            alignItems: 'center',
+            justifyContent: 'center',
+            marginHorizontal: 30,
+          }}
+        >
+          <Image
+            source={player.isTrackPlaying ? icons.pause : icons.play}
+            style={{
+              height: 25,
+              width: 25,
+              tintColor: COLORS.black,
+            }}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity activeOpacity={0.7}>
+          <Image
+            style={{ height: 25, width: 25, tintColor: COLORS.white }}
+            source={icons.next}
+          />
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   )
 }
 
-export default TrackPlayer
+export default TrackPlayerScreen
