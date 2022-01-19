c04691fbceb4530c903b443f63858f9dfb087b87
andres alcocer <andresalcocer7@yahoo.com>
2022-01-19 14:22:57 -0500

    refactored browseSlice

--- store/slices/browseSlice.ts
+++ store/slices/browseSlice.ts
@@ -1,9 +1,5 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit'
 import { CATEGORY_ITEMS } from '../../constants'
-import { BASE_URL } from '@env'
-
-import { setHeaders } from '../../utils/helpers'
-import { RootState } from '../index'
 
 const initialState = {
   categories: [{ id: '', name: '', description: '', images: [{ url: '' }] }],
@@ -14,30 +10,6 @@ const initialState = {
   rock: [{ description: '', images: [{ url: '' }] }],
 }
 
-export const getBrowseCategoriesPlaylistsAsync = createAsyncThunk<
-  any,
-  { limit: string; id: string },
-  { state: RootState }
->(
-  'browse/getCategoriesPlaylists',
-  async (args, { getState, rejectWithValue }) => {
-    const accessToken = getState().auth.accessToken
-    try {
-      const response = await fetch(
-        `${BASE_URL}/browse/categories/${args.id}/playlists?limit=${args.limit}`,
-        {
-          method: 'GET',
-          headers: setHeaders(accessToken),
-        }
-      )
-      const data = await response.json()
-      return { items: data.playlists.items, category: args.id }
-    } catch (error) {
-      rejectWithValue(error)
-    }
-  }
-)
-
 const browseSlice = createSlice({
   name: 'browse',
   initialState,
@@ -45,17 +17,6 @@ const browseSlice = createSlice({
     getBrowseCategories: (state) => {
       state.categories = CATEGORY_ITEMS
     },
-  },
-  extraReducers: (builder) => {
-    builder.addCase(
-      getBrowseCategoriesPlaylistsAsync.fulfilled,
-      (state, action) => {
-        return {
-          ...state,
-          [action.payload.category]: action.payload.items,
-        }
-      }
-    )
   },
 })
 
